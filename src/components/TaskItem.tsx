5195ba2a773d3deb3adeed61468eded4cb2d3123
Anton B <antonbsa.bck@gmail.com>
2025-03-19 23:05:20 -0300

    feat: enhance TaskItem component with completed state styling

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,4 @@
-import { Trash } from 'phosphor-react'
+import { Trash, Check } from 'phosphor-react'
 import styles from './TaskItem.module.css';
 
 interface TaskProps {
@@ -7,14 +7,20 @@ interface TaskProps {
   isCompleted: boolean;
 }
 
-export function TaskItem({ title }: TaskProps) {
+export function TaskItem({ title, isCompleted }: TaskProps) {
+  const checkboxStyle = `${styles.checkbox} ${isCompleted ? styles.checkboxCompleted : ''}`;
+  const titleStyle = `${styles.title} ${isCompleted ? styles.titleCompleted : ''}`;
+  const deleteStyle = `${styles.delete} ${isCompleted ? styles.deleteCompleted : ''}`;
+  
   return (
     <div className={styles.task}>
-      <button className={styles.checkbox} role="checkbox" />
-      <p className={styles.title}>
+      <button className={checkboxStyle} role="checkbox">
+        {isCompleted && <Check className={styles.checkCompleted} size={24} weight='bold' />}
+      </button>
+      <p className={titleStyle}>
         {title}
       </p>
-      <button className={styles.delete} type="button">
+      <button className={deleteStyle} type="button">
         <Trash size={18} weight="bold" />
       </button>
     </div>
